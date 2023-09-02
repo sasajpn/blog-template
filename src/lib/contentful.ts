3260eea9ac50b4de711f853ff5e6cb41f06b2f5e
kazuki_sasahara <kazuki.sasahara.07@gmail.com>
2023-09-02 12:09:35 +0900

    install nextjs

---
+++ src/lib/contentful.ts
@@ -0,0 +1,8 @@
+import { createClient, ContentfulClientApi } from "contentful";
+
+const client: ContentfulClientApi = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID as string,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+});
+
+export default client;
